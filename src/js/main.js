309b2bdd7fd6c53b1db7fcc4a74e5d921acd005e
Vadym <leshchenko.vadym.n@gmail.com>
2025-02-08 22:01:05 +0200

    finish second slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".main-slider .owl-carousel").owlCarousel({
         loop:true,
         margin:10,
         dots: true,
@@ -9,17 +9,31 @@ $(document).ready(function(){
         autoplayTimeout:5000,
         autoplayHoverPause: true,
         smartSpeed: 1500,
-        // dots: true,
-        // navElement: [
-        //     "<img src='../img/icons/prev.svg' alt=''>",
-        //     // "<img src='../img/icons/prev.svg' alt=''>"
-        // ]
     });
-    $(".custom-prev").click(function() {
+    $(".main-slider__custom-prev").click(function() {
         $(".owl-carousel").trigger("prev.owl.carousel");
     });
 
-    $(".custom-next").click(function() {
+    $(".main-slider__custom-next").click(function() {
+        $(".owl-carousel").trigger("next.owl.carousel");
+    });
+    $(".news__owl-carousel").owlCarousel({
+        margin: 73,
+        loop: true,
+        dots: true,
+        nav: false,
+        items: 3,
+        responsive: {
+            0: { items: 1 },
+            600: { items: 2 },
+            1000: { items: 3 }
+        },
+    });
+    $(".news__custom-prev").click(function() {
+        $(".owl-carousel").trigger("prev.owl.carousel");
+    });
+
+    $(".news__custom-next").click(function() {
         $(".owl-carousel").trigger("next.owl.carousel");
     });
   });
